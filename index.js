5e5c22a8e5394f112704d2f854ce5c9e0ecb2891
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-07 01:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 // Use Object.assign to assign key and value to a new object.
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  cloneObject = Object.assign({}, object, {key, value});
+  return cloneObject
+}
