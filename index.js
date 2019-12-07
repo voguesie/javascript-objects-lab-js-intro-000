aaa173a5f1186fba9bb7e70fcd16c235e0f2f7c2
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-07 02:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var cloneObject = Object.assign({}, object, {[key]: value});
   return cloneObject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
