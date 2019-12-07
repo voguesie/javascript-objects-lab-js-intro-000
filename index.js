47027b1260d01373e0142d70224c62c7d44e6906
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-07 02:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var cloneObject = object;
-  return delete cloneKey[key]
+  return delete cloneObject[key]
 }
